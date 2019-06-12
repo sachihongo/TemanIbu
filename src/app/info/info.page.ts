dba44382100ed74e0de4223d8c5d07850f8023de
sachihongo <sachihongo.sh@gmail.com>
2019-06-12 11:28:01 +0700

    Adding layout to info and promo page

--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ExploreService} from '../service/explore.service';
 
 @Component({
   selector: 'app-info',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info.page.scss'],
 })
 export class InfoPage implements OnInit {
+  news: any;
+  newsTitle: string;
+  newsSubtitle: string;
+  newsImageurl: string;
+  newsDescription: string;
 
-  constructor() { }
+
+  constructor(private exploreService: ExploreService) { }
 
   ngOnInit() {
+    this.exploreService.read_News().subscribe(data => {
+
+      this.news = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          Title: e.payload.doc.data()['Title'],
+          Subtitle: e.payload.doc.data()['Subtitle'],
+          Imageurl: e.payload.doc.data()['Imageurl'],
+          Description: e.payload.doc.data()['Description'],
+        };
+      })
+      console.log(this.news);
+
+    });
   }
 
 }
