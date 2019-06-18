9e25c8a28446bcee3342521200f4576e8205f43b
sachihongo <sachihongo.sh@gmail.com>
2019-06-18 11:46:28 +0700

    Adding change to profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import {AuthService} from '../service/auth.service';
+import {AngularFirestore, AngularFirestoreDocument} from "@angular/fire/firestore";
+import {UserService} from "../service/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
@@ -10,13 +13,28 @@ import {AuthService} from '../service/auth.service';
 export class ProfilePage implements OnInit {
   userEmail: string;
 
-  constructor(private menuCtrl: MenuController, private authService: AuthService) { }
+  mainuser: AngularFirestoreDocument;
+  sub;
+  username: String;
+  // profilePic: String;
 
-  ngOnInit() {
-    if (this.authService.userDetails()) {
-      this.userEmail = this.authService.userDetails().email;
-      console.log(this.userEmail);
-    }
+  constructor(private menuCtrl: MenuController, private authService: AuthService, private user: UserService, private router: Router, private afs: AngularFirestore) {
+    this.mainuser = afs.doc(`users/${user.getUID()}`)
+    this.sub = this.mainuser.valueChanges().subscribe(event => {
+      this.username = event.username
+      // this.profilePic = event.profilePic
+    })
+  }
+
+  // ngOnInit() {
+  //   if (this.authService.userDetails()) {
+  //     this.userEmail = this.authService.userDetails().email;
+  //     console.log(this.userEmail);
+  //   }
+  // }
+
+  goTo(postID: String) {
+    this.router.navigate(['/tabs/post/' + postID.split('/')[0]]);
   }
 
 }
